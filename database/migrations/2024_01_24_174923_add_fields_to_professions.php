dc442de0be9d83497e874b5a3329ccbb0d08e6d1
Alejandro <alexmonty6@gmail.com>
2024-01-24 18:55:51 +0100

    Ejercicio 1: Ampliar la tabla de profesiones

---
+++ database/migrations/2024_01_24_174923_add_fields_to_professions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToProfessions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('professions', function (Blueprint $table) {
+            $table->string('description')->after('title');
+            $table->string('education_level');
+            $table->integer('salary');
+            $table->string('sector');
+            $table->integer('experience_required');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('professions', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
