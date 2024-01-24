48fd50f039090315cb2e97f39f552e41bf2716c6
Alejandro <alexmonty6@gmail.com>
2024-01-24 19:53:01 +0100

    Ejercicio 4: Crear un listado de profesiones (1’5 puntos).

--- resources/views/professions/index.blade.php
+++ resources/views/professions/index.blade.php
@@ -9,32 +9,40 @@
 
     <table class="table">
         <thead class="thead-dark">
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Título</th>
-            <th scope="col">Perfiles</th>
-            <th scope="col">Acciones</th>
-        </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Título</th>
+                <th scope="col">Perfiles</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Nivel de educación</th>
+                <th scope="col">Salario</th>
+                <th scope="col">Experiencia requerida</th>
+                <th scope="col">Acciones</th>
+            </tr>
         </thead>
         <tbody>
-        @foreach($professions as $profession)
-            <tr>
-                <td scope="row">{{ $profession->id }}</td>
-                <td>{{ $profession->title }}</td>
-                <td>{{ $profession->profiles_count }}</td>
-                <td>
-                    @if($profession->profiles_count == 0)
-                        <form action="{{ url('profesiones/' . $profession->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-link"><span
+            @foreach ($professions as $profession)
+                <tr>
+                    <td scope="row">{{ $profession->id }}</td>
+                    <td>{{ $profession->title }}</td>
+                    <td>{{ $profession->profiles_count }}</td>
+                    <td>{{ $profession->sector }}</td>
+                    <td>{{ $profession->education_level }}</td>
+                    <td>{{ $profession->salary }}</td>
+                    <td>{{ $profession->experience_required }}</td>
+                    <td>
+                        @if ($profession->profiles_count == 0)
+                            <form action="{{ url('profesiones/' . $profession->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-link"><span
                                         class="material-symbols-outlined">delete</span>
-                            </button>
-                        </form>
-                    @endif
-                </td>
-            </tr>
-        @endforeach
+                                </button>
+                            </form>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 @endsection
